983b7c8354d8f97a17a6274c62db54a3c1639b1f
10samarth <samarthmg@gmail.com>
2023-03-02 17:59:20 -0800

    backend apis

---
+++ backend/server.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const filepath = "product-fixtures.json"
+
+// Get all products
+app.get('/api/products', (req, res) => {
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+      } else {
+        const products = JSON.parse(data);
+        res.json(products);
+      }
+    });
+  });
+  
+// Get a product by SKU
+app.get('/api/products/:sku', (req, res) => {
+    const productSku = req.params.sku;
+  
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+      } else {
+        const products = JSON.parse(data);
+        const product = products.find(p => p.sku === productSku);
+  
+        if (product) {
+          res.json(product);
+        } else {
+          res.status(404).send('Product not found');
+        }
+      }
+    });
+  });
+  
+  
+  // Update a product by SKU
+app.put('/api/products/:sku', (req, res) => {
+    const productSku = req.params.sku;
+    const updatedProductData = req.body;
+  
+    console.log("req:",req.params)
+    console.log("req.body:",req.body)
+
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+      } else {
+        const products = JSON.parse(data);
+        const productIndex = products.findIndex(p => p.sku === productSku);
+  
+        if (productIndex !== -1) {
+          products[productIndex] = { ...products[productIndex], ...updatedProductData };
+  
+          fs.writeFile(filepath, JSON.stringify(products), err => {
+            if (err) {
+              res.status(500).send('Error writing file');
+            } else {
+              res.json(products[productIndex]);
+            }
+          });
+        } else {
+          res.status(404).send('Product not found');
+        }
+      }
+    });
+  });
+  
+  // Delete a product by SKU
+app.delete('/api/products/:sku', (req, res) => {
+    const productSku = req.params.sku;
+  
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading file');
+      } else {
+        let products = JSON.parse(data);
+        const productIndex = products.findIndex(p => p.sku === productSku);
+  
+        if (productIndex !== -1) {
+          products.splice(productIndex, 1);
+  
+          fs.writeFile(filepath, JSON.stringify(products), err => {
+            if (err) {
+              res.status(500).send('Error writing file');
+            } else {
+              res.sendStatus(204);
+            }
+          });
+        } else {
+          res.status(404).send('Product not found');
+        }
+      }
+    });
+  });
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
